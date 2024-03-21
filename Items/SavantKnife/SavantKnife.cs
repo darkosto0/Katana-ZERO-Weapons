7dd258f20ddc57eb1b0621140caad0291a07046f
Darkosto <152528354+darkosto0@users.noreply.github.com>
2024-03-21 18:46:27 +0100

    Added savant knife

---
+++ Items/SavantKnife/SavantKnife.cs
@@ -0,0 +1,112 @@
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace KatanaZERO.Items.SavantKnife
+{
+    public class SavantKnife : ModItem
+    {
+        public static readonly SoundStyle Slash1 = new SoundStyle("KatanaZERO/Sounds/Items/SavantKnife/savant_slash1");
+        public static readonly SoundStyle Slash2 = new SoundStyle("KatanaZERO/Sounds/Items/SavantKnife/savant_slash2");
+        public static readonly SoundStyle Slash3 = new SoundStyle("KatanaZERO/Sounds/Items/SavantKnife/savant_slash3");
+
+        private float attackCooldown = 0f;
+        public bool hasAttacked = false;
+
+        public override void SetDefaults()
+        {
+            Item.damage = 76;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.rare = ItemRarityID.Purple;
+            Item.value = Item.sellPrice(gold: 10, silver: 24);
+            Item.UseSound = Slash1;
+            Item.crit = 15;
+
+            Item.useTime = 20;
+            Item.useAnimation = 1;
+
+            Item.useStyle = ItemUseStyleID.Rapier;
+            Item.holdStyle = ItemHoldStyleID.HoldRadio;
+            Item.holdStyle = 0;
+            Item.knockBack = 6;
+
+            Item.autoReuse = false;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.shoot = ModContent.ProjectileType<SavantSlash>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Katana, 1);
+            recipe.AddIngredient(ItemID.ChlorophyteBar, 10);
+            recipe.AddIngredient(ItemID.SoulofMight, 3);
+            recipe.AddIngredient(ItemID.SoulofSight, 3);
+            recipe.AddIngredient(ItemID.SoulofFright, 3);
+            recipe.AddTile(TileID.SharpeningStation);
+            recipe.Register();
+        }
+
+        public override bool? UseItem(Player player)
+        {
+
+            Vector2 direction = Main.MouseWorld - player.position;
+            direction.Normalize();
+
+            player.direction = direction.X > 0 ? 1 : -1;
+
+            if (hasAttacked)
+            {
+                player.velocity = direction * 3f;
+            }
+            else
+            {
+                player.velocity = direction * 12f;
+                hasAttacked = true;
+            }
+
+
+            attackCooldown = 15f; //artificial cooldown
+
+            System.Random random = new System.Random();
+            int randomNumber = random.Next(1, 4);
+            switch (randomNumber)
+            {
+                case 1:
+                    Item.UseSound = Slash1;
+                    break;
+                case 2:
+                    Item.UseSound = Slash2;
+                    break;
+                case 3:
+                    Item.UseSound = Slash3;
+                    break;
+            }
+            return true;
+        }
+        public override void HoldItem(Player player)
+        {
+            if (attackCooldown > 0f)
+            {
+                attackCooldown -= 1f;
+            }
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return attackCooldown <= 0f;
+        }
+        public override void UpdateInventory(Player player)
+        {
+            if (player.velocity.Y == 0f)
+            {
+                hasAttacked = false;
+            }
+        }
+    } 
+}
+
