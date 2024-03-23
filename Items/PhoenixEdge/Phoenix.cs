610dabdbd7f4bd3c0fc4c5c275260c9efb6cfc42
Darkosto <trajkovdarkotrajkov@gmail.com>
2024-03-23 17:26:20 +0100

    Added Pheonix Edge
    Flames not implemented yet

---
+++ Items/PhoenixEdge/Phoenix.cs
@@ -0,0 +1,110 @@
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace KatanaZERO.Items.PhoenixEdge
+{
+    public class Phoenix : ModItem
+    {
+        public static readonly SoundStyle Slash1 = new SoundStyle("KatanaZERO/Sounds/Items/PhoenixEdge/phoenix_slash1");
+        public static readonly SoundStyle Slash2 = new SoundStyle("KatanaZERO/Sounds/Items/PhoenixEdge/phoenix_slash2");
+        public static readonly SoundStyle Slash3 = new SoundStyle("KatanaZERO/Sounds/Items/PhoenixEdge/phoenix_slash3");
+
+        private float attackCooldown = 0f;
+        public bool hasAttacked = false;
+
+        public override void SetDefaults()
+        {
+            Item.damage = 145;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.rare = ItemRarityID.LightRed;
+            Item.value = Item.sellPrice(gold: 17, silver: 79);
+            Item.UseSound = Slash1;
+            Item.crit = 70;
+
+            Item.useTime = 20;
+            Item.useAnimation = 1;
+
+            Item.useStyle = ItemUseStyleID.Rapier;
+            Item.holdStyle = ItemHoldStyleID.HoldRadio;
+            Item.knockBack = 6;
+
+            Item.autoReuse = false;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.shoot = ModContent.ProjectileType<PhoenixSlash>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(Mod, "ZerosKatana");
+            recipe.AddIngredient(ItemID.FragmentSolar, 10);
+            recipe.AddIngredient(ItemID.LivingFireBlock, 5);
+            recipe.AddIngredient(ItemID.CursedFlame, 3);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.Register();
+        }
+
+        public override bool? UseItem(Player player)
+        {
+
+            Vector2 direction = Main.MouseWorld - player.position;
+            direction.Normalize();
+
+            player.direction = direction.X > 0 ? 1 : -1;
+
+            if (hasAttacked)
+            {
+                player.velocity = direction * 5f;
+            }
+            else
+            {
+                player.velocity = direction * 12f;
+                hasAttacked = true;
+            }
+
+
+            attackCooldown = 35f; //artificial cooldown
+
+            System.Random random = new System.Random();
+            int randomNumber = random.Next(1, 4);
+            switch (randomNumber)
+            {
+                case 1:
+                    Item.UseSound = Slash1;
+                    break;
+                case 2:
+                    Item.UseSound = Slash2;
+                    break;
+                case 3:
+                    Item.UseSound = Slash3;
+                    break;
+            }
+            return true;
+        }
+        public override void HoldItem(Player player)
+        {
+            if (attackCooldown > 0f)
+            {
+                attackCooldown -= 1f;
+            }
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return attackCooldown <= 0f;
+        }
+        public override void UpdateInventory(Player player)
+        {
+            if (player.velocity.Y == 0f)
+            {
+                hasAttacked = false;
+            }
+        }
+    } 
+}
+
