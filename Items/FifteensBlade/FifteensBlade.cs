51a33ac60dbd059dd90c5cffb4cb2100283b5206
Darkosto <trajkovdarkotrajkov@gmail.com>
2024-05-08 23:22:34 +0200

    we're back
    just took a month off, dont know why, but i feel the urge to finish this thing now for some reason.
    I forgot to commit a lot of changes so im doing this right now

---
+++ Items/FifteensBlade/FifteensBlade.cs
@@ -0,0 +1,113 @@
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace KatanaZERO.Items.FifteensBlade
+{
+    public class FifteensBlade : ModItem
+    {
+        public static readonly SoundStyle Slash1 = new SoundStyle("KatanaZERO/Sounds/Items/FifteensBlade/fifteen_slash1");
+        public static readonly SoundStyle Slash2 = new SoundStyle("KatanaZERO/Sounds/Items/FifteensBlade/fifteen_slash2");
+        public static readonly SoundStyle Slash3 = new SoundStyle("KatanaZERO/Sounds/Items/FifteensBlade/fifteen_slash3");
+
+        private float attackCooldown = 0f;
+        public bool hasAttacked = false;
+
+        public override void SetDefaults()
+        {
+            Item.damage = 9999999;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.rare = ItemRarityID.Master;
+            Item.value = Item.sellPrice(gold: 99, silver: 99);
+            Item.UseSound = Slash1;
+            Item.crit = 100;
+
+            Item.useTime = 20;
+            Item.useAnimation = 1;
+
+            Item.useStyle = ItemUseStyleID.Rapier;
+            Item.holdStyle = ItemHoldStyleID.HoldRadio;
+            Item.knockBack = 6;
+
+            Item.autoReuse = false;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.shoot = ModContent.ProjectileType<FifteensSlash>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(Mod, "ZerosKatana");
+            recipe.AddIngredient(ItemID.FragmentSolar, 20);
+            recipe.AddIngredient(ItemID.FragmentStardust, 20);
+            recipe.AddIngredient(ItemID.FragmentVortex, 20);
+            recipe.AddIngredient(ItemID.FragmentNebula, 20);
+            recipe.AddIngredient(ItemID.LunarOre, 50);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.Register();
+        }
+
+        public override bool? UseItem(Player player)
+        {
+
+            Vector2 direction = Main.MouseWorld - player.position;
+            direction.Normalize();
+
+            player.direction = direction.X > 0 ? 1 : -1;
+
+            if (hasAttacked)
+            {
+                player.velocity = direction * 5f;
+            }
+            else
+            {
+                player.velocity = direction * 12f;
+                hasAttacked = true;
+            }
+
+
+            attackCooldown = 20f; //artificial cooldown
+
+            System.Random random = new System.Random();
+            int randomNumber = random.Next(1, 4);
+            switch (randomNumber)
+            {
+                case 1:
+                    SoundEngine.PlaySound(new SoundStyle("KatanaZERO/Sounds/Items/FifteensBlade/fifteen_slash1"));
+                    break;
+                case 2:
+                    SoundEngine.PlaySound(new SoundStyle("KatanaZERO/Sounds/Items/FifteensBlade/fifteen_slash2"));
+                    break;
+                case 3:
+                    SoundEngine.PlaySound(new SoundStyle("KatanaZERO/Sounds/Items/FifteensBlade/fifteen_slash3"));
+                    break;
+            }
+            return true;
+        }
+        public override void HoldItem(Player player)
+        {
+            if (attackCooldown > 0f)
+            {
+                attackCooldown -= 1f;
+            }
+        }
+        public override bool CanUseItem(Player player)
+        {
+                return attackCooldown <= 0f;
+        }
+        public override void UpdateInventory(Player player)
+        {
+            if (player.velocity.Y == 0f)
+            {
+                hasAttacked = false;
+            }
+        }
+
+    }
+}
+
