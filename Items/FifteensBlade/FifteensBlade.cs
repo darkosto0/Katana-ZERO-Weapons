b9eeeb8b6d6c5dbea203f621ecef11de0a9151f4
Darkosto <trajkovdarkotrajkov@gmail.com>
2024-05-13 22:32:54 +0200

    Wokring towards Fifteen's sword
    I tried making this today but i only got so far, I might release the mod without it finished, but I'll see

--- Items/FifteensBlade/FifteensBlade.cs
+++ Items/FifteensBlade/FifteensBlade.cs
@@ -3,6 +3,9 @@ using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using System;
+using KatanaZERO.Dusts;
 
 namespace KatanaZERO.Items.FifteensBlade
 {
@@ -13,11 +16,14 @@ namespace KatanaZERO.Items.FifteensBlade
         public static readonly SoundStyle Slash3 = new SoundStyle("KatanaZERO/Sounds/Items/FifteensBlade/fifteen_slash3");
 
         private float attackCooldown = 0f;
+        private float dragonCooldown = 0f;
+
         public bool hasAttacked = false;
+        public bool hasRightClicked = false;
 
         public override void SetDefaults()
         {
-            Item.damage = 9999999;
+            Item.damage = 620;
             Item.DamageType = DamageClass.Melee;
             Item.width = 40;
             Item.height = 40;
@@ -36,6 +42,7 @@ namespace KatanaZERO.Items.FifteensBlade
             Item.autoReuse = false;
             Item.noUseGraphic = true;
             Item.noMelee = true;
+
             Item.shoot = ModContent.ProjectileType<FifteensSlash>();
         }
 
@@ -54,7 +61,6 @@ namespace KatanaZERO.Items.FifteensBlade
 
         public override bool? UseItem(Player player)
         {
-
             Vector2 direction = Main.MouseWorld - player.position;
             direction.Normalize();
 
@@ -73,7 +79,7 @@ namespace KatanaZERO.Items.FifteensBlade
 
             attackCooldown = 20f; //artificial cooldown
 
-            System.Random random = new System.Random();
+            System.Random random = new Random();
             int randomNumber = random.Next(1, 4);
             switch (randomNumber)
             {
@@ -89,16 +95,55 @@ namespace KatanaZERO.Items.FifteensBlade
             }
             return true;
         }
+
         public override void HoldItem(Player player)
         {
             if (attackCooldown > 0f)
             {
                 attackCooldown -= 1f;
             }
+
+            if (Main.mouseRight)
+            {
+                int dustAmount = 800; // Total number of dust particles in the circle
+                float radius = 464f; // Radius for the circle
+                int dustType = ModContent.DustType<FifteenDust>(); // Dust type
+
+                for (int i = 0; i < dustAmount; i++)
+                {
+                    float angle = MathHelper.TwoPi / dustAmount * i;
+                    Vector2 position = player.Center + radius * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                    Dust dustCircle = Dust.NewDustPerfect(position, dustType);
+                    dustCircle.noGravity = true;
+                    dustCircle.noLight = true;
+                }
+
+                float distanceToCursor = Vector2.Distance(player.Center, Main.MouseWorld);
+                float maxLerpAmount = Math.Min(1f, radius / distanceToCursor);
+
+                int dustLineAmount = (int)Math.Round(distanceToCursor / 3);
+
+                for (int i = 0; i <= dustLineAmount; i++)
+                {
+                    float lerpAmount = (float)i / dustLineAmount;
+                    if (lerpAmount > maxLerpAmount) break;
+
+                    Vector2 barPosition = Vector2.Lerp(player.Center, Main.MouseWorld, lerpAmount);
+                    if (!Collision.SolidCollision(barPosition, 10, 10))
+                    {
+                        Dust dust = Dust.NewDustPerfect(barPosition, dustType);
+                        dust.noGravity = true;
+                    }
+                }
+                player.velocity = Vector2.Zero;
+            }
+
+
         }
+
         public override bool CanUseItem(Player player)
         {
-                return attackCooldown <= 0f;
+            return attackCooldown <= 0f;
         }
         public override void UpdateInventory(Player player)
         {
@@ -107,7 +152,6 @@ namespace KatanaZERO.Items.FifteensBlade
                 hasAttacked = false;
             }
         }
-
     }
 }
 
